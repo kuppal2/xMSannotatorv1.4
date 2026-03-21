530be3651bb0046e32af6ba369edada35eb6d54f
karan uppal <kuppal3gt@gmail.com>
2026-03-21 19:23:19 -0400

    xMSannotator v1.4.1

--- R/run_cpp_metabolomics_engine.R
+++ R/run_cpp_metabolomics_engine.R
@@ -69,9 +69,9 @@ function(dataA,
  # E(g)$weight <- pmax(E(g)$weight^2, 0)
   set.seed(555)
 
- #  cl <- cluster_infomap(g, e.weights = (E(g)$weight)) #resolution = resolution
+  cl <- cluster_infomap(g, e.weights = abs(E(g)$weight)) #resolution = resolution
 
-   cl <- cluster_walktrap(g,steps=30) #, weights = E(g)$weight, steps = 5)
+   #cl <- cluster_walktrap(g,steps=30) #, weights = E(g)$weight, steps = 5)
 
   dataA[, Module_RTclust := as.character(membership(cl))]
 
